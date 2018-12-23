1b8f8cacd6f0c2884170d2e8df842831ea0d8c8d
Andrew Huth <andrew@huth.me>
2018-12-23 05:45:26 -0500

    Document randomizer

--- src/utils/randomizer.js
+++ src/utils/randomizer.js
@@ -1,13 +1,39 @@
 import { range, shuffle } from 'lodash';
 
+/**
+ * @typedef {number[]} Randomizer
+ */
+
+/**
+ * @typedef {array} ReturnValue
+ * @property {number} 0 - Next number
+ * @property {Randomizer} 1 - Next randomizer
+ */
+
+/**
+ * Create a new randomizer object, which is passed in to the other methods in this module to get
+ * random numbers.
+ * @returns {Randomizer}
+ */
 export function create() {
   return shuffle(range(7));
 }
 
+/**
+ * See the next number.
+ * @param {Randomizer} randomizer
+ * @returns {number}
+ */
 export function peek(randomizer) {
   return randomizer[0];
 }
 
+/**
+ * Get the next number and a new randomizer.
+ * @param {Randomizer} randomizer
+ * @returns {ReturnValue}
+ * @see http://tetris.wikia.com/wiki/Random_Generator
+ */
 export function next(randomizer) {
   const [head, ...tail] = randomizer;
 
