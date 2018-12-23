3b23266fe787934d0c8ed0a05906171352118610
Andrew Huth <andrew@huth.me>
2018-12-23 06:12:37 -0500

    Document tetromino

--- src/utils/tetromino.js
+++ src/utils/tetromino.js
@@ -1,3 +1,18 @@
+/**
+ * @typedef {Object} Tetromino
+ * @property {string} type
+ * @property {number} x
+ * @property {number} y
+ * @property {number} orientation 
+ */
+
+/**
+ * Create a new Tetromino.
+ * @param {string} type 
+ * @param {number} x 
+ * @param {number} y 
+ * @returns {Tetromino}
+ */
 export function create(type, x, y) {
   return {
     type,
@@ -7,6 +22,11 @@ export function create(type, x, y) {
   };
 }
 
+/**
+ * Rotate a Tetromino clockwise.
+ * @param {Tetromino} piece 
+ * @returns {Tetromino}
+ */
 export function rotate(piece) {
   return {
     ...piece,
