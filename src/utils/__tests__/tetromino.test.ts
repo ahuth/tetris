faad99ea7b3af635c23e4ea98bf20e066d5936a8
Andrew Huth <andrew.huth@airbnb.com>
2019-03-22 09:27:31 -0700

    Look up once and capture the shape used in a test

--- src/utils/__tests__/tetromino.test.ts
+++ src/utils/__tests__/tetromino.test.ts
@@ -2,18 +2,20 @@ import { shapes, ShapeType, ShapeOrientation } from '../shapes';
 import * as Tetromino from '../tetromino';
 
 test('rotate', () => {
+  const expectedShape = shapes[ShapeType.L];
+
   let piece = Tetromino.create(ShapeType.L);
-  expect(Tetromino.getShape(piece)).toEqual(shapes.L[ShapeOrientation.Up]);
+  expect(Tetromino.getShape(piece)).toEqual(expectedShape[ShapeOrientation.Up]);
 
   piece = Tetromino.rotate(piece);
-  expect(Tetromino.getShape(piece)).toEqual(shapes.L[ShapeOrientation.Right]);
+  expect(Tetromino.getShape(piece)).toEqual(expectedShape[ShapeOrientation.Right]);
 
   piece = Tetromino.rotate(piece);
-  expect(Tetromino.getShape(piece)).toEqual(shapes.L[ShapeOrientation.Down]);
+  expect(Tetromino.getShape(piece)).toEqual(expectedShape[ShapeOrientation.Down]);
 
   piece = Tetromino.rotate(piece);
-  expect(Tetromino.getShape(piece)).toEqual(shapes.L[ShapeOrientation.Left]);
+  expect(Tetromino.getShape(piece)).toEqual(expectedShape[ShapeOrientation.Left]);
 
   piece = Tetromino.rotate(piece);
-  expect(Tetromino.getShape(piece)).toEqual(shapes.L[ShapeOrientation.Up]);
+  expect(Tetromino.getShape(piece)).toEqual(expectedShape[ShapeOrientation.Up]);
 });
