1f5e48492d9a765bbd4364be5dac983555be363c
Andrew Huth <andrew.huth@airbnb.com>
2019-03-22 09:25:28 -0700

    Use enum values instead of hardcoded ones

--- src/utils/__tests__/tetromino.test.ts
+++ src/utils/__tests__/tetromino.test.ts
@@ -1,19 +1,19 @@
-import { shapes, ShapeType } from '../shapes';
+import { shapes, ShapeType, ShapeOrientation } from '../shapes';
 import * as Tetromino from '../tetromino';
 
 test('rotate', () => {
   let piece = Tetromino.create(ShapeType.L);
-  expect(Tetromino.getShape(piece)).toEqual(shapes.L[0]);
+  expect(Tetromino.getShape(piece)).toEqual(shapes.L[ShapeOrientation.Up]);
 
   piece = Tetromino.rotate(piece);
-  expect(Tetromino.getShape(piece)).toEqual(shapes.L[1]);
+  expect(Tetromino.getShape(piece)).toEqual(shapes.L[ShapeOrientation.Right]);
 
   piece = Tetromino.rotate(piece);
-  expect(Tetromino.getShape(piece)).toEqual(shapes.L[2]);
+  expect(Tetromino.getShape(piece)).toEqual(shapes.L[ShapeOrientation.Down]);
 
   piece = Tetromino.rotate(piece);
-  expect(Tetromino.getShape(piece)).toEqual(shapes.L[3]);
+  expect(Tetromino.getShape(piece)).toEqual(shapes.L[ShapeOrientation.Left]);
 
   piece = Tetromino.rotate(piece);
-  expect(Tetromino.getShape(piece)).toEqual(shapes.L[0]);
+  expect(Tetromino.getShape(piece)).toEqual(shapes.L[ShapeOrientation.Up]);
 });
