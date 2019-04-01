ae96ab9b3ba5b4515b5ec668cfe71c952cc39b1b
Andrew Huth <andrew.huth@airbnb.com>
2019-03-31 18:50:01 -0700

    Add rotation of pieces

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Piece from './Piece';
 import { ShapeType } from '../utils/shapes';
 import * as Tetromino from '../utils/tetromino';
 
 export default function App() {
-  const tetromino = Tetromino.create(ShapeType.Z);
+  const [tetromino, setTetromino] = React.useState(Tetromino.create(ShapeType.Z));
+
+  useEffect(() => {
+    function handleKeyUp(event: KeyboardEvent) {
+      if (event.key === 'ArrowUp') {
+        setTetromino(Tetromino.rotate(tetromino));
+      }
+    }
+
+    document.addEventListener('keyup', handleKeyUp);
+
+    return () => document.removeEventListener('keyup', handleKeyUp);
+  });
 
   return (
     <Piece tetromino={tetromino} />
