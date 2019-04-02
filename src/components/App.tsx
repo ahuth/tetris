d87b694ac9de543f2629fa8d9ab401d3e6025287
Andrew Huth <andrew@huth.me>
2019-04-01 22:13:53 -0700

    Revert "For real fix rotating the test piece"
    This reverts commit 6822b1ea42e99560f0b326b191ae69f4ebe90677.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,33 +2,22 @@ import React from 'react';
 import Piece from './Piece';
 import * as Tetromino from '../utils/tetromino';
 
-const initialPiece = Tetromino.create(Tetromino.ShapeType.Z);
-
-function reducer(state: Tetromino.Tetromino, action: { type: string }) {
-  switch (action.type) {
-    case 'rotate':
-      return Tetromino.rotate(state);
-    default:
-      return state;
-  }
-}
-
 export default function App() {
-  const [state, dispatch] = React.useReducer(reducer, initialPiece);
-
-  function handleKeyUp(event: KeyboardEvent) {
-    if (event.key === 'ArrowUp') {
-      dispatch({ type: 'rotate' });
-    }
-  }
+  const [tetromino, setTetromino] = React.useState(Tetromino.create(Tetromino.ShapeType.Z));
 
   React.useEffect(() => {
+    function handleKeyUp(event: KeyboardEvent) {
+      if (event.key === 'ArrowUp') {
+        setTetromino(Tetromino.rotate(tetromino));
+      }
+    }
+
     document.addEventListener('keyup', handleKeyUp);
 
     return () => document.removeEventListener('keyup', handleKeyUp);
   }, []);
 
   return (
-    <Piece tetromino={state} />
+    <Piece tetromino={tetromino} />
   );
 }
