c5fac9ab2df5794733d6a52f587e32400761014c
Andrew Huth <andrew@huth.me>
2019-04-02 03:01:04 -0700

    Avoid recomputing starting tetromino

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,8 +3,10 @@ import Piece from './Piece';
 import useKeyHandler from '../hooks/useKeyHandler';
 import * as Tetromino from '../utils/tetromino';
 
+const initialTetromino = Tetromino.create(Tetromino.ShapeType.Z);
+
 export default function App() {
-  const [tetromino, setTetromino] = React.useState(Tetromino.create(Tetromino.ShapeType.Z));
+  const [tetromino, setTetromino] = React.useState(initialTetromino);
 
   useKeyHandler('ArrowUp', () => {
     setTetromino(current => Tetromino.rotate(current));
