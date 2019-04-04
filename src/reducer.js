ffc962cfa0075ab544dd76913aaf972a62151a94
Andrew Huth <andrew@huth.me>
2019-04-03 19:48:57 -0700

    Add remaining state values

--- src/reducer.js
+++ src/reducer.js
@@ -1,3 +1,4 @@
+import { fill } from 'lodash';
 import * as Point from './utils/point';
 import * as Randomizer from './utils/randomizer';
 import * as Tetromino from './utils/tetromino';
@@ -6,7 +7,9 @@ const [firstShape, randomizer] = Randomizer.next(Randomizer.create());
 
 export const initialState = {
   current: Tetromino.create(firstShape),
+  filled: fill(new Array(200), 0, 0),
   level: 1,
+  paused: true,
   position: Point.create(3, 0),
   randomizer: randomizer,
   score: 0,
