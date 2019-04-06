7aa92a0bd97e3da980c88eae70d1a77ed59373f2
Andrew Huth <andrew@huth.me>
2019-04-06 11:24:29 -0700

    Tweak function comments

--- src/utils/board.js
+++ src/utils/board.js
@@ -8,7 +8,7 @@ export function create() {
   return fill(new Array(200), 0, 0);
 }
 
-// Move left, if possible. Either way, return a position.
+// Move a tetromino left, if possible. Either way, return a position.
 export function moveLeft(board, tetromino, position) {
   if (canMoveLeft(board, tetromino, position)) {
     return Point.create(position.x - 1, position.y)
@@ -16,7 +16,7 @@ export function moveLeft(board, tetromino, position) {
   return position;
 }
 
-// Move right, if possible. Either way, return a position.
+// Move a tetromino right, if possible. Either way, return a position.
 export function moveRight(board, tetromino, position) {
   if (canMoveRight(board, tetromino, position)) {
     return Point.create(position.x + 1, position.y)
