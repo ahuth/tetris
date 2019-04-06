23823ed3d0ef4972a9a724e36ac9175423593235
Andrew Huth <andrew@huth.me>
2019-04-06 11:28:20 -0700

    Tweak how the board functions work and the stub functions

--- src/utils/board.js
+++ src/utils/board.js
@@ -10,36 +10,36 @@ export function create() {
 
 // Move a tetromino left, if possible. Either way, return a position.
 export function moveLeft(board, tetromino, position) {
-  if (canMoveLeft(board, tetromino, position)) {
-    return Point.create(position.x - 1, position.y)
+  const potentialPosition = Point.create(position.x - 1, position.y);
+
+  if (isValid(board, potentialPosition, position)) {
+    return potentialPosition;
   }
   return position;
 }
 
 // Move a tetromino right, if possible. Either way, return a position.
 export function moveRight(board, tetromino, position) {
-  if (canMoveRight(board, tetromino, position)) {
-    return Point.create(position.x + 1, position.y)
+  const potentialPosition = Point.create(position.x + 1, position.y);
+
+  if (isValid(board, potentialPosition, position)) {
+    return potentialPosition;
   }
   return position;
 }
 
 // Rotate a tetromino, if possible. Either way, return a tetromino.
 export function rotate(board, tetromino, position) {
-  if (canRotate(board, tetromino, position)) {
+  const potentialTetromino = Tetromino.rotate(tetromino);
+
+  if (isValid(board, potentialTetromino, position)) {
     return Tetromino.rotate(tetromino);
   }
   return tetromino;
 }
 
-function canMoveLeft(board, tetromino, position) {
-  return true;
-}
-
-function canMoveRight(board, tetromino, position) {
-  return true;
-}
-
-function canRotate(board, tetromino, position) {
+// Determine if a tetromino at a certain position on the board is "valid". In other words, doesn't
+// overlap with any existing filled spots and is completely on the board.
+function isValid(board, tetromino, position) {
   return true;
 }
