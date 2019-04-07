b7ef0a0b4f55b612c421396b8a37e5bd6d67ca51
Andrew Huth <andrew.huth@airbnb.com>
2019-04-07 13:16:36 -0700

    Updates

---
+++ precache-manifest.b2f684817ca97e571446fec3ada21c36.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2e3a053d3a774332234e",
+    "url": "/tetris/static/css/main.73e18855.chunk.css"
+  },
+  {
+    "revision": "2e3a053d3a774332234e",
+    "url": "/tetris/static/js/main.455c8c31.chunk.js"
+  },
+  {
+    "revision": "e9e58439d3b8c64431c2",
+    "url": "/tetris/static/js/runtime~main.61707ba2.js"
+  },
+  {
+    "revision": "f97499677ea5ef87f988",
+    "url": "/tetris/static/js/2.7835fab0.chunk.js"
+  },
+  {
+    "revision": "e29d749cecfa29e891e99815684803f9",
+    "url": "/tetris/index.html"
+  }
+];
