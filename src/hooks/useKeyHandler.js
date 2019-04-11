2581c5d486dc1aeab4b52be70d4420ccee11acee
Andrew Huth <andrew@huth.me>
2019-04-10 23:01:39 -0700

    Use correct key event name

--- src/hooks/useKeyHandler.js
+++ src/hooks/useKeyHandler.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 export default function useKeyHandler(key, handler) {
   React.useEffect(() => {
-    function handleKeyUp(event) {
+    function handleKeyDown(event) {
       if (event.key === key) {
         handler(event);
       }
     }
 
-    document.addEventListener('keydown', handleKeyUp);
+    document.addEventListener('keydown', handleKeyDown);
 
-    return () => document.removeEventListener('keyup', handleKeyUp);
+    return () => document.removeEventListener('keydown', handleKeyDown);
   }, [key]);
 }
