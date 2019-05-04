a50d851bead6d7daf7ae9821ab2c085eb4150fdf
Andrew Huth <andrew@huth.me>
2019-05-03 18:41:28 -0700

    Give <MainBoard> the capability of displaying arbitrary content
    Useful for things like paused or gameover menus.

--- src/components/Paused.jsx
+++ src/components/Paused.jsx
@@ -6,10 +6,9 @@ import { ActionTypes } from '../reducer';
 export default function Paused({ dispatch, state }) {
   return (
     <div style={styles.container}>
-      <div style={styles.inner}>
-        <MainBoard board={state.board} current={state.current} position={state.position} />
+      <MainBoard board={state.board} current={state.current} position={state.position}>
         <span style={styles.text}>Paused</span>
-      </div>
+      </MainBoard>
       <ScoreBoard level={state.level} next={state.next} score={state.score}>
         <button onClick={() => dispatch(ActionTypes.Start)}>Start</button>
       </ScoreBoard>
@@ -20,9 +19,6 @@ export default function Paused({ dispatch, state }) {
 const styles = {
   container: {
     display: 'flex',
-  },
-  inner: {
-    position: 'relative',
   },
   text: {
     backgroundColor: 'white',
