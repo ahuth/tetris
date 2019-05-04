5d8f99ed115eb35d81493796f3989e193a75ca54
Andrew Huth <andrew@huth.me>
2019-05-03 18:11:57 -0700

    Extract <Playing> component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,30 +1,11 @@
 import React from 'react';
-import MainBoard from './MainBoard';
-import ScoreBoard from './ScoreBoard';
-import useKeyHandler from '../hooks/useKeyHandler';
-import useInterval from '../hooks/useInterval';
-import reducer, { initialState, ActionTypes } from '../reducer';
+import Playing from './Playing';
+import reducer, { initialState } from '../reducer';
 
 export default function App() {
   const [state, dispatch] = React.useReducer(reducer, initialState);
 
-  useKeyHandler('ArrowUp', () => dispatch(ActionTypes.Rotate));
-  useKeyHandler('ArrowDown', () => dispatch(ActionTypes.MoveDown));
-  useKeyHandler('ArrowLeft', () => dispatch(ActionTypes.MoveLeft));
-  useKeyHandler('ArrowRight', () => dispatch(ActionTypes.MoveRight));
-
-  useInterval(state.interval, () => dispatch(ActionTypes.Tick));
-
   return (
-    <div style={styles.container}>
-      <MainBoard board={state.board} current={state.current} position={state.position} />
-      <ScoreBoard level={state.level} next={state.next} score={state.score} />
-    </div>
+    <Playing dispatch={dispatch} state={state} />
   );
 }
-
-const styles = {
-  container: {
-    display: 'flex',
-  },
-};
