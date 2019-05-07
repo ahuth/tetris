3d1af80be7e5a5a721998c001852147e71086b8d
Andrew Huth <andrew@huth.me>
2019-05-06 21:40:23 -0700

    Remove unused Point function

--- src/utils/point.ts
+++ src/utils/point.ts
@@ -8,7 +8,3 @@ interface Point {
 export function create(x: number, y: number): Point {
   return { x, y };
 }
-
-export function equals(a: Point, b: Point): boolean {
-  return a.x === b.x && a.y === b.y;
-}
