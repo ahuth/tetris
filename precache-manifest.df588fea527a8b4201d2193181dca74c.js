90ec431bfeaedc1e25ab695b9889d586a4368388
Andrew Huth <andrew@huth.me>
2019-05-12 15:50:23 -0700

    Updates

---
+++ precache-manifest.df588fea527a8b4201d2193181dca74c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b8e4548480d48a262fec16616e3e718",
+    "url": "/tetris/index.html"
+  },
+  {
+    "revision": "2a8a88b2307e9eecc244",
+    "url": "/tetris/static/css/main.2e3ac2e6.chunk.css"
+  },
+  {
+    "revision": "cf129f7b0b0668c09f6d",
+    "url": "/tetris/static/js/2.a9172611.chunk.js"
+  },
+  {
+    "revision": "2a8a88b2307e9eecc244",
+    "url": "/tetris/static/js/main.f5258dc7.chunk.js"
+  },
+  {
+    "revision": "e9e58439d3b8c64431c2",
+    "url": "/tetris/static/js/runtime~main.61707ba2.js"
+  }
+]);
