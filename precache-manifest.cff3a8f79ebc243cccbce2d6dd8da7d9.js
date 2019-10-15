e42c407927fa4ac3abf8eacff3c4356f6f1e78be
Andrew Huth <andrew@huth.me>
2019-10-14 22:01:26 -0700

    Updates

---
+++ precache-manifest.cff3a8f79ebc243cccbce2d6dd8da7d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "856c3a4f6b246ed3fed05db956a579ed",
+    "url": "/tetris/index.html"
+  },
+  {
+    "revision": "7437cc009b579c233459",
+    "url": "/tetris/static/css/main.2e3ac2e6.chunk.css"
+  },
+  {
+    "revision": "acedb32a07990528dd06",
+    "url": "/tetris/static/js/2.892d1b70.chunk.js"
+  },
+  {
+    "revision": "7437cc009b579c233459",
+    "url": "/tetris/static/js/main.f61fc388.chunk.js"
+  },
+  {
+    "revision": "96caf6de5370c7eb0c2c",
+    "url": "/tetris/static/js/runtime-main.5086e28a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/tetris/precache-manifest.a45145e83d74f2190e1f1e89f9ff1dfd.js"
+  "/tetris/precache-manifest.cff3a8f79ebc243cccbce2d6dd8da7d9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tetris/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
